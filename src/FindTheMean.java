218fbc313ea322b56a353eb99125d3b5d25e3df0
Reginald Eli A. Deinla <aldrineg@Reginalds-MacBook-Pro.local>
2013-02-02 19:00:05 +0800

    Passed!

--- src/FindTheMean.java
+++ src/FindTheMean.java
@@ -1,60 +1,83 @@
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 import java.util.Scanner;
+import java.util.SortedSet;
 import java.util.TreeSet;
 
 public class FindTheMean {
+	
+	public static void addToValues(Map<Long, Integer> values, long v){
+		if (values.get(v) == null){
+			values.put(v, 1);
+		}else{
+			values.put(v, values.get(v) + 1);
+		}
+	}
+	
+	public static boolean inValues(Map<Long, Integer> values, long v){
+		Integer value = values.get(v);
+		return value != null && value > 0;
+	}
+	
+	public static void removeFromValues(Map<Long, Integer> values, long v){
+		Integer value = values.get(v);
+		if (value != null && value > 0){
+			values.put(v, value-1);
+		}
+	}
 
 	public static long findMin(long n, long k, long a, long b, long c, long r) {
 		n = ((n - k + k) % (k + 1)) + k + 1;
 
 		List<Long> kValues = new ArrayList<Long>((int) k);
+		Map<Long, Integer> values = new HashMap<Long, Integer>();
 		TreeSet<Long> valueSet = new TreeSet<Long>();
 
 		kValues.add(a);
+		addToValues(values, a);
 		valueSet.add(a);
 
 		long lastVal = a;
 
-		List<Long> poss = new ArrayList<Long>();
+		TreeSet<Long> poss = new TreeSet<Long>();
 
-		for (int i = 0; i < k + 1; i++) {
+		for (int i = 0; i < k + 2; i++) {
 			poss.add((long) i);
 		}
 
 		for (int j = 1; j < k; j++) {
 			lastVal = ((b * lastVal) + c) % r;
-			if (lastVal < 0){
-				throw new RuntimeException("NEGATIVE!");
-			}
 			kValues.add(lastVal);
+			addToValues(values, lastVal);
 			valueSet.add(lastVal);
-			if (poss.get(0) == lastVal) {
-				poss.add(poss.remove(0));
-			}
 		}
 		
-		System.out.println("GENERATED INITIAL");
+		
+		TreeSet<Long> skipped = new TreeSet<Long>();
 
-		for (int j = (int) k; j < n/* k + k + 1 */; j++) {
-			for (long l = poss.get(0); poss.size() > 0; l = poss.get(0)) {
-				if (!valueSet.contains(l)) {
-					poss.remove(0);
+		for (int j = (int) k; j < n; j++) {
+			
+			for (long l = poss.pollFirst(); poss.size() > 0; l = poss.pollFirst()) {
+				if (!inValues(values, l)) {
 					long toRemove = kValues.remove(0);
-					if (!kValues.contains(toRemove)) {
+					removeFromValues(values, toRemove);
+					if (!inValues(values, toRemove)){
 						valueSet.remove(toRemove);
-					}
-					if (poss.contains(toRemove) && toRemove < poss.get(0)
-							&& !valueSet.contains(toRemove)) {
-						poss.remove(toRemove);
-						poss.add(0, toRemove);
+						if (skipped.size() > 0){
+							if (skipped.contains(toRemove)){
+								SortedSet<Long> tail = skipped.tailSet(toRemove, true);
+								poss.addAll(tail);
+								tail.clear();
+							}
+						}
 					}
 					lastVal = l;
 					kValues.add(lastVal);
-					valueSet.add(lastVal);
 					break;
 				} else {
-					poss.add(poss.remove(0));
+					skipped.add(l);
 				}
 			}
 		}
